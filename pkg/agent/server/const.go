80f59f92fa42173e0ba0773ab083a36312cb746a
Yousong Zhou <zhouyousong@yunionyun.com>
2018-07-24 07:42:58 +0000

    server: longer idle check and servers flow refresh rate

--- pkg/agent/server/const.go
+++ pkg/agent/server/const.go
@@ -6,6 +6,6 @@ import (
 
 const (
 	DefaultHostConfigPath    string        = "/etc/yunion/host.conf"
-	FlowManIdleCheckDuration time.Duration = 5 * time.Second
-	WatcherRefreshRate       time.Duration = 7 * time.Second
+	FlowManIdleCheckDuration time.Duration = 13 * time.Second
+	WatcherRefreshRate       time.Duration = 31 * time.Second
 )
