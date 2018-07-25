2f849c68a876c1d49cd7167e47ad58c2c8dc2f3e
Yousong Zhou <zhouyousong@yunionyun.com>
2018-07-25 09:48:24 +0000

    sdncli: add short descriptions

--- cmd/sdncli/cmd/dumpBridgePort.go
+++ cmd/sdncli/cmd/dumpBridgePort.go
@@ -23,13 +23,8 @@ import (
 // dumpBridgePortCmd represents the dumpBridgePort command
 var dumpBridgePortCmd = &cobra.Command{
 	Use:   "dumpBridgePort",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Dump OpenFlow port number",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		cli.DoCmd(cmd)
 	},
