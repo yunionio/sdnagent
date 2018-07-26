a089dd9008ab145c023aceed1325c9ad71f18fb2
Yousong Zhou <zhouyousong@yunionyun.com>
2018-07-26 12:58:58 +0000

    utils: flowsource: rework for traffic between local VMs

--- pkg/agent/utils/flowsource.go
+++ pkg/agent/utils/flowsource.go
@@ -77,16 +77,16 @@ func (h *HostLocal) FlowsMap() (map[string][]*ovs.Flow, error) {
 		F(0, 29300, "tcp,nw_dst=169.254.169.254,tp_dst=80",
 			T("mod_dl_dst:{{.MAC}},mod_nw_dst:{{.IP}},mod_tp_dst:{{.MetadataPort}},LOCAL")),
 		F(0, 27200, "in_port=LOCAL", "normal"),
-		F(0, 27100, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}}"), "normal"),
+		F(0, 26900, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}}"), "normal"),
 	)
-	if !h.HostConfig.AllowSwitchVMs {
+	if h.HostConfig.AllowSwitchVMs {
 		flows = append(flows,
-			F(0, 25600, T("in_port={{.PortNoPhy}},dl_dst=01:00:00:00:00:00/01:00:00:00:00:00"), "normal"),
-			F(0, 25500, T("in_port={{.PortNoPhy}}"), "drop"),
+			F(0, 24700, T("in_port={{.PortNoPhy}}"), "normal"),
 		)
 	} else {
 		flows = append(flows,
-			F(0, 25700, T("in_port={{.PortNoPhy}}"), "normal"),
+			F(0, 24600, T("in_port={{.PortNoPhy}},dl_dst=01:00:00:00:00:00/01:00:00:00:00:00"), "normal"),
+			F(0, 24500, T("in_port={{.PortNoPhy}}"), "drop"),
 		)
 	}
 	return map[string][]*ovs.Flow{h.Bridge: flows}, nil
@@ -131,8 +131,8 @@ func (g *Guest) FlowsMap() (map[string][]*ovs.Flow, error) {
 				T("mod_dl_dst:{{.MAC}},mod_nw_src:169.254.169.254,mod_tp_src:80,output:{{.PortNo}}")),
 			F(0, 28400, T("in_port={{.PortNo}},udp,tp_src=68,tp_dst=67"), "local"),
 			F(0, 28300, T("in_port=LOCAL,dl_dst={{.MAC}},udp,tp_src=67,tp_dst=68"), T("output:{{.PortNo}}")),
-			F(0, 25900, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}},{{._dl_vlan}}"), "normal"),
-			F(0, 25800, T("in_port={{.PortNo}}"), "normal"),
+			F(0, 26700, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}},{{._dl_vlan}}"), "normal"),
+			F(0, 25700, T("in_port={{.PortNo}}"), "normal"),
 		}
 		flows = append(flows, g.SecurityRules.Flows(m)...)
 		if fs, ok := r[nic.Bridge]; ok {
@@ -149,32 +149,33 @@ func (g *Guest) FlowsMap() (map[string][]*ovs.Flow, error) {
 func (sr *SecurityRules) Flows(data map[string]interface{}) []*ovs.Flow {
 	T := t(data)
 	data["_in_port_vm"] = "reg0=0x10000/0x10000"
+	data["_in_port_not_vm"] = "reg0=0x0/0x10000"
 	loadReg0BitVm := "load:0x1->NXM_NX_REG0[16]" // "0x1->" is important, not "1->"
 	loadZone := fmt.Sprintf("load:0x%x->NXM_NX_REG0[0..15]", data["CT_ZONE"])
-	loadZonePhy := fmt.Sprintf("load:0x%x->NXM_NX_REG0[0..15]", 1000)
+	loadZoneDstVM := fmt.Sprintf("load:0x%x->NXM_NX_REG1[0..15]", data["CT_ZONE"])
 
 	flows := []*ovs.Flow{}
 	// table 0
 	// table 1 sec_CT
 	flows = append(flows,
 		F(0, 27300, T("in_port=LOCAL,dl_dst={{.MAC}},ip"),
-			T("ct(table=1,zone={{.CT_ZONE}})")),
-		F(0, 26900, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}},{{._dl_vlan}},ip,ct_state=-trk"),
-			loadZonePhy+T(",ct(table=1,zone={{.CT_ZONE}})")),
-		F(0, 26800, T("in_port={{.PortNo}},ip,ct_state=-trk"),
+			loadZone+T(",ct(table=1,zone={{.CT_ZONE}})")),
+		F(0, 26800, T("in_port={{.PortNoPhy}},dl_dst={{.MAC}},{{._dl_vlan}},ip"),
+			loadZone+T(",ct(table=1,zone={{.CT_ZONE}})")),
+		F(0, 25800, T("in_port={{.PortNo}},ip"),
 			loadReg0BitVm+","+loadZone+T(",ct(table=1,zone={{.CT_ZONE}})")),
 		// ct_state= flags order matters
-		F(1, 7900, T("ip,ct_zone={{.CT_ZONE}},ct_state=+inv+trk"), "drop"),
-		F(1, 7800, T("ip,ct_zone={{.CT_ZONE}},ct_state=+new+trk,{{._in_port_vm}}"), "resubmit(,2)"),
-		F(1, 7700, T("ip,ct_zone={{.CT_ZONE}},ct_state=+new+trk"), "resubmit(,3)"),
-		F(1, 7600, T("ip,ct_zone={{.CT_ZONE}}"), "normal"),
+		F(1, 7900, "ip,ct_state=+inv+trk", "drop"),
+		F(1, 7800, T("ip,ct_state=+new+trk,{{._in_port_not_vm}}"), "resubmit(,3)"),
+		F(1, 7700, T("ip,ct_state=+new+trk,{{._in_port_vm}}"), "resubmit(,2)"),
+		F(1, 7600, "ip", "resubmit(,4)"),
+		F(4, 5600, T("ip,dl_dst={{.MAC}}"), loadZoneDstVM+",resubmit(,5),"),
+		F(4, 5500, "ip", "ct(commit,zone=NXM_NX_REG0[0..15]),normal"),
 	)
 
 	// table sec_CT_OUT
-	//
-	// out is must to allow resubmit(,sec_CT_IN)
 	prioOut := 40000
-	matchOut := T("in_port={{.PortNo}},ip")
+	matchOut := T("in_port={{.PortNo}}")
 	for _, r := range sr.outRules {
 		if prioOut <= 20 {
 			log.Errorf("%s: %q generated too out rules",
@@ -190,18 +191,11 @@ func (sr *SecurityRules) Flows(data map[string]interface{}) []*ovs.Flow {
 			prioOut -= 1
 		}
 	}
-	// NOTE the rule is mainly for debugging purposes: counting packets
-	// dropped for no matching in the out pipeline
-	flows = append(flows, F(2, 20, matchOut, "drop"))
 
 	// table sec_CT_IN
-	//
-	// in is also a must to forward packets from sec_CT_OUT
-	//
-	// NOTE assume MAC is unique across the platform
 	prioIn := 40000
 	matchIn := T("dl_dst={{.MAC}}")
-	actionAllowIn := T("ct(commit,zone=NXM_NX_REG0[0..15]),ct(commit,zone={{.CT_ZONE}}),normal")
+	actionAllowIn := loadZoneDstVM + ",resubmit(,5)"
 	for _, r := range sr.inRules {
 		if prioIn <= 30 {
 			log.Errorf("%s: %q generated too many in rules",
@@ -217,14 +211,24 @@ func (sr *SecurityRules) Flows(data map[string]interface{}) []*ovs.Flow {
 			prioIn -= 1
 		}
 	}
+	// NOTE Traffics enter sec_XX table by dl_dst=MAC_VM, except the egress
+	// rule in_port=PORT_VM.  The following rule are for VM accessing hosts
+	// other than locally managed VMs
+	flows = append(flows, F(3, 30, "ip", "ct(commit,zone=NXM_NX_REG0[0..15]),normal"))
+	flows = append(flows,
+		F(5, 20, T("ip,{{._in_port_not_vm}}"), "ct(commit,zone=NXM_NX_REG1[0..15]),normal"),
+		F(5, 10, T("ip,{{._in_port_vm}}"), "ct(commit,zone=NXM_NX_REG1[0..15]),ct(commit,zone=NXM_NX_REG0[0..15]),normal"),
+	)
 	return flows
 }
 
 // NOTE: KEEP THIS IN SYNC WITH CODE ABOVE
 //
+//	grep -oE '\<F\([0-9].*' pkg/agent/utils/flowsource.go  | sort -k 1.3,1.4n -k2r
+//
 // Assumptions
 //
-//  - MAC is unique, IP is not
+//  - MAC is unique, IP is not.  IP can be unique we hold the ground now
 //  - We are the only user of ct_zone other than 0
 //
 // Flows with "VM" in them are guest nic-specific flows
@@ -236,32 +240,41 @@ func (sr *SecurityRules) Flows(data map[string]interface{}) []*ovs.Flow {
 // 29200 metaserver_resp_VM_IP,actions=mod_metaserver_PORT_VM
 // 28400 in_port=PORT_VM,dhcp_req,actions=LOCAL
 // 28300 in_port=LOCAL,dhcp_resp,actions=PORT_VM
-// 27300 in_port=LOCAL,dl_dst=MAC_VM,ip,ct_state=-trk,actions=load_ZONE,ct(zone=ZONE,table=sec_IN)
-// 27200 in_port=LOCAL,actions=normal
-// 27100 in_port=PORT_PHY,dl_dst=MAC_PHY,actions=normal
 //
-// 26900 in_port=PORT_PHY,dl_dst=MAC_VM,dl_vlan=VLAN_VM,ip,ct_state=-trk,actions=load_ZONE,load_PHY_BIT,ct(zone=ZONE,table=sec_CT)
-// 26800 in_port=PORT_VM,ip,ct_state=-trk,actions=load_ZONE,load_VM_BIT,ct(zone=ZONE,table=sec_CT)
-// 25900 in_port=PORT_PHY,dl_dst=MAC_VM,dl_vlan=VLAN_VM,actions=normal
-// 25800 in_port=PORT_VM,actions=normal
-// 25700 in_port=PORT_PHY,{ allow_switch_vms},actions=normal
-// 25600 in_port=PORT_PHY,{!allow_switch_vms},dl_dst=01:00:00:00:00:00/01:00:00:00:00:00,actions=normal
-// 25500 in_port=PORT_PHY,{!allow_switch_vms},actions=drop
+// 27300 in_port=LOCAL,dl_dst=MAC_VM,ip,actions=load_dst_VM_ZONE,ct(zone=dst_VM_ZONE,table=sec_CT)
+// 27200 in_port=LOCAL,actions=normal
+//
+// 26900 in_port=PORT_PHY,dl_dst=MAC_PHY,actions=normal
+// 26800 in_port=PORT_PHY,dl_dst=MAC_VM,dl_vlan=VLAN_VM,ip,actions=load_dst_VM_ZONE,ct(zone=dst_VM_ZONE,table=sec_CT)
+// 26700 in_port=PORT_PHY,dl_dst=MAC_VM,dl_vlan=VLAN_VM,actions=normal
+//
+// 25800 in_port=PORT_VM,ip,actions=load_src_VM_ZONE,load_VM_BIT,ct(zone=src_VM_ZONE,table=sec_CT)
+// 25700 in_port=PORT_VM,actions=normal
+//
+// 24700 in_port=PORT_PHY,{ allow_switch_vms},actions=normal
+// 24600 in_port=PORT_PHY,{!allow_switch_vms},dl_dst=01:00:00:00:00:00/01:00:00:00:00:00,actions=normal
+// 24500 in_port=PORT_PHY,{!allow_switch_vms},actions=drop
 //
 // Table 1 sec_CT
-//  7900 ip,ct_zone=ZONE,ct_state=+trk+inv,actions=drop
-//  7800 ip,ct_zone=ZONE,ct_state=+trk+new,{{reg0_vm_set}},actions=resubmit(,sec_OUT)
-//  7700 ip,ct_zone=ZONE,ct_state=+trk+new,actions=resubmit(,sec_IN)
-//  7600 ip,ct_zone=ZONE,actions=normal
+//  7900 ip,ct_state=+trk+inv,actions=drop
+//  7800 ip,ct_state=+trk+new,{{!in_port_vm}},actions=resubmit(,sec_IN)
+//  7700 ip,ct_state=+trk+new,{{ in_port_vm}},actions=resubmit(,sec_OUT)
+//  7600 actions=normal
 //
 // Table 2 sec_OUT
 // 40000 in_port=PORT_VM,match_allow,actions=resubmit(,sec_IN)
 //   ... in_port=PORT_VM,match_deny,actions=drop
-//    20 in_port=PORT_VM,,actions=drop
 //
 // Table 3 sec_IN
-// 40000 dl_dst=MAC_VM,match_allow,actions=ct(commit,zone=ZONE),normal
-//   ... dl_dst=MAC_VM,match_deny,,actions=drop
-//    30 ip,actions="ct(commit,zone=ZONE_REG),normal"
+// 40000 dl_dst=MAC_VM,match_allow,actions=load_dst_VM_ZONE,resubmit(,sec_CT_commit)
+//   ... dl_dst=MAC_VM,match_deny,actions=drop
+//    30 ip,actions=ct(commit,zone=reg0_ZONE),normal
 //
-// grep -oE '\<F\([0-9].*' pkg/agent/utils/flowsource.go  | sort -k 1.3,1.4n -k2r
+// Table 4 sec_CT_OkayEd
+//  5600 ip,dl_dst=MAC_VM,actions=load_dst_VM_ZONE,resubmit(,sec_CT_commit)
+//  5500 ip,actions=ct(commit,zone=reg0_ZONE),normal
+//
+// Table 5 sec_CT_commit
+//    20 ip,{!in_port_vm},actions=ct(commit,zone=dst_VM_ZONE),normal
+//    10 ip,{ in_port_vm},actions=commit(commit,zone=src_VM_ZONE),ct(commit,zone=dst_VM_ZONE),normal
+//     0 drop
