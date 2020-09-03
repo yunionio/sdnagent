fd4cf95a3ae0afbd8036a2ccb58d9e2e35b3586c
Yousong Zhou <zhouyousong@yunionyun.com>
2020-09-03 20:53:57 +0800

    build: eipgw: vrrp track script as monitor

--- build/sdnagent/root/usr/share/sdnagent/ansible/files/keepalived-eipgw-notify.sh
+++ build/sdnagent/root/usr/share/sdnagent/ansible/files/keepalived-eipgw-notify.sh
@@ -1,28 +1,55 @@
 #!/usr/bin/env bash
 
 prog="$0"
-state="$1"
+statefile=/var/run/yunion-sdnagent-eipgw.state
+pidfile=/var/run/yunion-sdnagent-eipgw.pid
 
 
 __errmsg() {
 	logger -t "$prog" "$*"
 }
 
-__errmsg "got state $state"
+_start() {
+	/opt/yunion/bin/sdnagent --config /etc/yunion/sdnagent.conf 2>&1 | logger -t sdnagent-eipgw &
+	__errmsg "sdnagent eipgw process group id $$"
+}
 
-set -x
-case "$state" in
-	MASTER)
-		/opt/yunion/bin/sdnagent --config /etc/yunion/sdnagent.conf 2>&1 | logger -t sdnagent-eipgw &
-		__errmsg "sdnagent eipgw process group id $$"
-		;;
-	*)
-		pkill --pidfile '/var/run/yunion-sdnagent-eipgw.pid'
-		ovs-vsctl --if-exists del-br breip
-		ovs-vsctl list-ports brvpc \
-			| grep '^ev-' \
-			| while read port; do \
-				ovs-vsctl --if-exists del-port brvpc "$port"; \
-			done
-		;;
-esac
+notify() {
+	local state="$1"; shift
+
+	__errmsg "got state $state"
+
+	set -x
+	echo "$state" >"$statefile"
+	case "$state" in
+		MASTER)
+			_start
+			;;
+		*)
+			pkill --pidfile "$pidfile"
+			ovs-vsctl --if-exists del-br breip
+			ovs-vsctl list-ports brvpc \
+				| grep '^ev-' \
+				| while read port; do \
+					ovs-vsctl --if-exists del-port brvpc "$port"; \
+				done
+			;;
+	esac
+}
+
+monitor() {
+	local state
+	if [ -s "$statefile" ]; then
+		state="$(<"$statefile")"
+		if [ "$state" = MASTER ]; then
+			local comm
+			comm="$(pgrep --list-name --pidfile "$pidfile" | cut -d' ' -f2)"
+			if [ -z "$comm" -o "$comm" != sdnagent ]; then
+				_start
+			fi
+		fi
+	fi
+	return 0
+}
+
+"$@"
