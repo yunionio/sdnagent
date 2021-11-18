ed03b2e26c10e262ab05eaaf44d5a2762673484d
Qiu Jian <qiujian@yunionyun.com>
2021-11-19 03:03:01 +0800

    feature: add tcman option switch

--- pkg/agent/server/hostlocal.go
+++ pkg/agent/server/hostlocal.go
@@ -56,6 +56,9 @@ func (hl *HostLocal) updateFlows(ctx context.Context) {
 }
 
 func (hl *HostLocal) updateTc(ctx context.Context) {
+	if hl.watcher.tcMan == nil {
+		return
+	}
 	data := []*utils.TcData{}
 	for _, hcn := range hl.watcher.hostConfig.HostNetworkConfigs() {
 		td := &utils.TcData{
